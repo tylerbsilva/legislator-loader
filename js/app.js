e0c8312806415d65b26a58cabaaa6958bc92a504
Tyler Silva <tylerbsilva@gmail.com>
2015-06-03 14:25:07 -0500

    beautifying contents

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
 // Initialize app as object
 var tagSearch = {};
 
-tagSearch.getTags = function(search){
+tagSearch.getTags = function(search) {
   var params = {
     count: 20,
   };
 
-  var url = "https://api.instagram.com/v1/tags/"+search+"/media/recent?client_id=357dd76e8bb5452e888dfca32bcae0db";
+  var url = "https://api.instagram.com/v1/tags/" + search + "/media/recent?client_id=357dd76e8bb5452e888dfca32bcae0db";
 
   var response = $.ajax({
-    type: 'GET',
-    url: url,
-    data: params,
-    dataType: 'jsonp'
-  })
-  .done(function(result){
-    //console.log(result.data);
-    tagSearch.populateImages(result.data);
-  })
-  .fail(function(){
-    console.log('fail');
-  });
+      type: 'GET',
+      url: url,
+      data: params,
+      dataType: 'jsonp'
+    })
+    .done(function(result) {
+      //console.log(result.data);
+      tagSearch.populateImages(result.data);
+    })
+    .fail(function() {
+      console.log('fail');
+    });
 };
 
-tagSearch.populateImages = function(resultArray){
+tagSearch.populateImages = function(resultArray) {
   var html = "";
-  $.each(resultArray, function(index, element){
+  $.each(resultArray, function(index, element) {
     html += "<li>";
     html += "<img src='" + element.images.standard_resolution.url + "' alt='" + element.caption.text + "' />";
     html += "<blockquote>" + element.caption.text + "</blockquote>";
@@ -36,8 +36,8 @@ tagSearch.populateImages = function(resultArray){
 };
 
 // When document loads, show this function
-$(document).ready(function(){
-  $('#get-tag').submit(function(event){
+$(document).ready(function() {
+  $('#get-tag').submit(function(event) {
     event.preventDefault();
     $('.resultsList').empty();
     var search = $('#tagSubmission').val();
