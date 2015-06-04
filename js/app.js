c06e989be06abb2941ad060de2580f96c6d8c37f
Tyler Silva <tylerbsilva@gmail.com>
2015-06-04 11:02:25 -0500

    Removed spaces if there are ones in search

--- js/app.js
+++ js/app.js
@@ -5,11 +5,8 @@ tagSearch.getTags = function(search) {
   var params = {
     count: 20,
   };
-
   search = tagSearch.removeHashtag(search);
-
   var url = "https://api.instagram.com/v1/tags/" + search + "/media/recent?client_id=357dd76e8bb5452e888dfca32bcae0db";
-
   var response = $.ajax({
       type: 'GET',
       url: url,
@@ -17,14 +14,15 @@ tagSearch.getTags = function(search) {
       dataType: 'jsonp'
     })
     .done(function(result) {
-      //console.log(result.data);
+      //console.log(result);
       tagSearch.populateImages(result.data);
     })
-    .fail(function() {
-      console.log('fail');
+    .fail(function(response) {
+      tagSearch.failed(response.error_message);
     });
 };
 
+// Pushing the data retreived to the DOM
 tagSearch.populateImages = function(resultArray) {
   var html = "";
   $.each(resultArray, function(index, element) {
@@ -38,9 +36,12 @@ tagSearch.populateImages = function(resultArray) {
 };
 
 // Remove Hash symbol from
-tagSearch.removeHashtag = function(searchTerm){
-  searchTerm = searchTerm.replace(" ", "");
-  if(searchTerm.charAt(0) == "#" || searchTerm.charAt(0) == "&#35;"){
+tagSearch.removeHashtag = function(searchTerm) {
+  // Remove space if there is one
+  searchTerm = searchTerm.replace(/ /g, "");
+
+  // Remove # if at the beginning of the search term
+  if (searchTerm.charAt(0) == "#" || searchTerm.charAt(0) == "&#35;") {
     searchTerm = searchTerm.substr(1);
     return searchTerm;
   } else {
@@ -48,14 +49,25 @@ tagSearch.removeHashtag = function(searchTerm){
   }
 };
 
+// display error message if failed
+tagSearch.failed = function(error){
+  var html = "<p>I'm sorry, we're experienceing issues with Instagram right now</p><p>Message: " + error + "</p>";
+  $(".resultsList").append(html);
+};
+
 
 // When document loads, show this function
 $(document).ready(function() {
+  // On submit of form
   $('#get-tag').submit(function(event) {
     event.preventDefault();
+    // Clear the list
     $('.resultsList').empty();
+    // Grab search tag
     var search = $('#tagSubmission').val();
+    // Clear search box
     $('#tagSubmission').val("");
+    // Run the search through the function to grab tags
     tagSearch.getTags(search);
   });
 });
